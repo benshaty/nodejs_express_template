2375484d3dc49b3ca5ae7b2a8ea68f9f96f07b68
ben <benshaty@gmail.com>
2021-08-05 08:09:45 +0300

    updated config file

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const config = {
     "port" : 4500,
     "locals" : {
-        name : "Node js express template",
-        title : "Template"
+        "name" : "Node js express template",
+        "title" : "Template"
     },
     "setupDb" : {
         "host": "localhost",
